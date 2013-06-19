adfca95249ffbc0b071cb9020d5d4de861ebeeae
Andrew Sackville-West <awest@janrain.com>
2013-06-19 13:29:21 -0700

    fiddle. distinguish types of data watchers. register ChildrenDataWatchers. some clean up

--- src/Database/Zookeeper/Monitor.hs
+++ src/Database/Zookeeper/Monitor.hs
@@ -1,5 +1,7 @@
 module Monitor ( registerDataWatcher
                , registerChildrenWatcher
+               , registerChildrenDataWatcher
+               , ZooMonitor
                ) where
 
 import Control.Applicative    ( (<$>) )
@@ -14,25 +16,36 @@ import Database.Zookeeper
 type Path = String
 
 type WatcherCallback a = a -> IO ()
+type DataCallback = WatcherCallback (Maybe ByteString)
+type ParentCallback = WatcherCallback ([Path], [Path])
 
 data Watcher
-     -- | a Zookeeper DataWatcher. Callback will be called with the path of the watched node upon event
-     = DataWatcher (WatcherCallback (Maybe ByteString))
+     -- | a Zookeeper DataWatcher. Callback will be called with the
+     -- most recent contents of the watched node upon event
+     = DataWatcher DataCallback
 
-     -- | a Zookeeper ChildrenWatcher. Callback will receive the current list of children of the watched node
-     | ChildrenWatcher [Path] (WatcherCallback ([Path], [Path]))
+     -- | a Zookeeper DataWatcher specifically for child nodes of a
+     -- ChildrenDataWater. These watchers are not directly refreshed,
+     -- but indirectly through the refresh of a ChildrenDataWatcher
+     | ChildDataWatcher DataCallback
+
+     -- | a Zookeeper ChildrenWatcher. Callback will receive a tuple
+     -- containing the list of new children and list of deleted
+     -- children of the watched node
+     | ChildrenWatcher [Path] ParentCallback
 
      -- | a hybrid of DataWatcher and ChildrenWatcher. The first
-     -- callback will receive the current list of children of the
-     -- watched node when a child node is created or deleted. The
-     -- second callback will receive the path of a child node as if a
-     -- DataWatcher had been applied to it. All child nodes will be
-     -- watched automatically, so this kind of watcher will respond to
-     -- any changes in the hierarchy.
-     | ChildDataWatcher [Path] (WatcherCallback ([Path], [Path])) (WatcherCallback (Maybe ByteString))
+     -- callback will receive a tuple containing the list of new
+     -- children and list of deleted children of the watched node when
+     -- a child node is created or deleted. The second callback will
+     -- receive the path of a child node as if a DataWatcher had been
+     -- applied to it. All child nodes will be watched automatically,
+     -- so this kind of watcher will respond to any changes in the
+     -- hierarchy.
+     | ChildrenDataWatcher [Path] ParentCallback DataCallback
 
      -- | a Zookeeper CreateWatcher which will be called with the path of the watched node when the node is created.
-     | CreateWatcher (WatcherCallback Path)
+     | CreateWatcher DataCallback
 
 type Watchers = HashMap Path [Watcher]
 
@@ -40,8 +53,11 @@ data ZooMonitor = ZooMonitor { zHandle :: ZHandle
                              , watchers :: IORef Watchers
                              }
 
-registerDataWatcher :: ZooMonitor -> Path -> (WatcherCallback (Maybe ByteString)) -> IO ()
-registerDataWatcher zm path fn = do
+registerDataWatcher :: ZooMonitor -> Path -> DataCallback -> IO ()
+registerDataWatcher = registerDataWatcher' DataWatcher
+
+registerDataWatcher' :: (DataCallback -> Watcher) -> ZooMonitor -> Path -> DataCallback -> IO ()
+registerDataWatcher' ctor zm path fn = do
   res <- try $ get (zHandle zm) path Watch
   case res of
     Right (val, _) -> do
@@ -49,7 +65,7 @@ registerDataWatcher zm path fn = do
       case attempt of
         Right () -> do
           atomicModifyIORef (watchers zm) $ \ws ->
-            let ws' = insertWith (++) path [DataWatcher fn] ws
+            let ws' = insertWith (++) path [ctor fn] ws
             in (ws' `seq` ws', ())
         Left e -> throw (e::SomeException)
     Left e@(ErrNoNode _) -> throw e -- node does not exist, how can we watch it? die...
@@ -60,20 +76,39 @@ registerDataWatcher zm path fn = do
                         -- back up (if it does) so register ourselves
                         -- for that event
 
-registerChildrenWatcher :: ZooMonitor -> Path -> (WatcherCallback ([Path], [Path])) -> IO ()
+registerChildDataWatcher :: ZooMonitor -> Path -> DataCallback -> IO ()
+registerChildDataWatcher = registerDataWatcher' ChildDataWatcher 
+
+
+registerChildrenWatcher :: ZooMonitor -> Path -> ParentCallback -> IO ()
 registerChildrenWatcher zm path fn = do
   res <- try $ getChildren (zHandle zm) path Watch
   case res of
     Right children -> do
       attempt <- try $ fn (children, [])
       case attempt of
-        Right () -> do
-          atomicModifyIORef (watchers zm) $ \ws ->
+        Right () -> atomicModifyIORef (watchers zm) $ \ws ->
             let ws' = insertWith (++) path [ChildrenWatcher children fn] ws
             in (ws' `seq` ws', ())
         Left e -> throw (e::SomeException)
     Left e@(ErrNoNode _) -> throw e
     Left _ -> undefined -- see registerDataWatcher ... we want the same thing here
+
+registerChildrenDataWatcher :: ZooMonitor -> Path -> ParentCallback -> DataCallback -> IO ()
+registerChildrenDataWatcher zm path pfn cfn = do
+  res <- try $ getChildren (zHandle zm) path Watch
+  case res of
+    Right children -> do
+      attempt <- try $ pfn (children, [])
+      case attempt of
+        Right () -> do
+          atomicModifyIORef (watchers zm) $ \ws ->
+            let ws' = insertWith (++) path [ChildrenDataWatcher children pfn cfn] ws
+            in (ws' `seq` ws', ())
+          forM_ children $ \child -> registerChildDataWatcher zm child cfn
+        Left e -> throw (e::SomeException)
+    Left e@(ErrNoNode _) -> throw e
+    Left _ -> undefined -- same as above
 
 watcher :: ZooMonitor -> ZHandle -> EventType -> State -> Path -> IO ()
 watcher zm zh event Connected path =
@@ -88,7 +123,7 @@ watcher _ _ _ Connecting _ = undefined -- wait for it...
 watcher _ _ _ _ _ = undefined -- other states that we might care about?
 
 ------------------------------------------------------------------------
--- handle a Changeed event for a data watch
+-- handle a Changed event for a data watch
 --
 -- if the get fails (meaning the node was deleted before we handled
 -- the change, is this possible?), then delete the watcher from our
@@ -103,14 +138,14 @@ handleChanged zm zh path = do
         let ws' = HashMap.delete path ws
         in (ws' `seq` ws', ())
       Left _ -> undefined -- what do we do here for different error types? queue this action?
-  where fns = dataWatchers . HashMap.lookupDefault [] path <$> readIORef (watchers zm) :: IO [WatcherCallback (Maybe ByteString)]
+  where fns = dataWatchers . HashMap.lookupDefault [] path <$> readIORef (watchers zm)
 
 
 ------------------------------------------------------------------------
 -- handle a Child event
---         
+--
 -- For a ChildrenWatcher, just compute the changes and call the
--- function. For a ChildDataWatcher, compute the changes, call the
+-- function. For a ChildrenDataWatcher, compute the changes, call the
 -- parent callback with the changes, then register a data watcher on
 -- each new child
 --
@@ -120,14 +155,13 @@ handleChildren zm zh path = do
     children <- getChildren zh path Watch
     ws >>= flip forM_ (applyWatcher children)
   where ws = HashMap.lookupDefault [] path <$> readIORef (watchers zm)
-        applyWatcher cs w = 
-          let childChanges known = (cs \\ known, known \\ cs) 
+        applyWatcher cs w =
+          let childChanges known = (cs \\ known, known \\ cs)
           in case w of
             ChildrenWatcher knownChildren fn -> fn $ childChanges knownChildren
-            ChildDataWatcher knownChildren pfn cfn -> do
-              let newChildren = cs \\ knownChildren
-                  deletedChildren = knownChildren \\ cs
-              pfn (newChildren, deletedChildren)
+            ChildrenDataWatcher knownChildren pfn cfn -> do
+              let cs'@(newChildren, _) = childChanges knownChildren
+              pfn cs'
               forM_ newChildren $ \child -> registerDataWatcher zm child cfn
             _ -> error "impossible handleChildren -- child watch event on improper Watcher"
 
@@ -136,7 +170,7 @@ handleChildren zm zh path = do
 -- handle a Deleted event
 --
 handleDeleted :: ZooMonitor -> Path -> IO ()
-handleDeleted zm path = atomicModifyIORef (watchers zm) $ \ws -> 
+handleDeleted zm path = atomicModifyIORef (watchers zm) $ \ws ->
   let ws' = HashMap.delete path ws
   in (ws' `seq` ws', ())
 
@@ -149,7 +183,7 @@ handleDeleted zm path = atomicModifyIORef (watchers zm) $ \ws ->
 
 ------------------------------------------------------------------------
 -- extract only the data watchers from a list of watchers
-dataWatchers :: [Watcher] -> [WatcherCallback (Maybe ByteString)]
+dataWatchers :: [Watcher] -> [DataCallback]
 dataWatchers [] = []
 dataWatchers ((DataWatcher cb) : ws) = cb : dataWatchers ws
 dataWatchers (_:ws) = dataWatchers ws
